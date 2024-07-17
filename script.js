0b9505e0943748520f1aff86622e2ca6f9c15bc0
Thanuja-1129 <madhunalathanuja@gmail.com>
2024-07-18 01:06:38 +0530

    completed till login with java script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let searchBtm
+function showlogin(){
+    let a = document.getElementById("login-container");
+    a.style.top='0';
+  }
+
+function removelogin(){
+    let a=document.getElementById("login-container");
+    a.style.top='-120%';
+}
+
