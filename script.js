15c66959ca718bc380caa094e959a59e81e93c25
Thanuja-1129 <madhunalathanuja@gmail.com>
2024-07-23 01:56:34 +0530

    completed without java script

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ function removelogin(){
     let a=document.getElementById("login-container");
     a.style.top='-120%';
 }
-
+document.getElementById('cbook').addEventListener('click', function() {
+window.scrollBy({
+top:151.18110236,
+behavior:'smooth'
+});
+});
