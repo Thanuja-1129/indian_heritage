c65cd56d11bff85c57cca816597aa3da26191b59
Thanuja-1129 <madhunalathanuja@gmail.com>
2024-08-07 22:10:06 +0530

    complete without back end

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-function showlogin(){
-    let a = document.getElementById("login-container");
-    a.style.top='0';
-  }
+let searchBtn = document.querySelector('#search-btn');
+let searchbar = document.querySelector('.search-bar-container');
+let loginform = document.querySelector('.login-form-container');
+let openlogin = document.querySelector('#login-btn');
+let closelogin= document.querySelector('#form-close');
 
-function removelogin(){
-    let a=document.getElementById("login-container");
-    a.style.top='-120%';
+openlogin.addEventListener('click',()=>{
+  loginform.style.top='0';
+})
+closelogin.addEventListener('click',()=>{
+  loginform.style.top='-120%';
+})
+searchBtn.addEventListener('click',() => {
+  searchBtn.classList.toggle('fa-times');
+  searchbar.classList.toggle('active');
+})
+window.onscroll=()=>{
+  searchBtn.classList.remove('fa-times');
+  searchbar.classList.remove('active');
 }
-document.getElementById('cbook').addEventListener('click', function() {
-window.scrollBy({
-top:151.18110236,
-behavior:'smooth'
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+window.onscroll = () =>{
+searchBtn.classList.remove('fa-times');
+searchBar.classList.remove('active');
+menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
+  
+}
+
+menu.addEventListener('click' , () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  });
+
+searchBtn.addEventListener('click' , () =>{
+searchBtn.classList.toggle('fa-times');
+searchBar.classList.toggle('active');
 });
+
+formBtn.addEventListener('click' , () =>{
+loginForm.classList.add('active');
 });
+
+formClose.addEventListener('click' , () =>{
+loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+     btn.addEventListener('click', ()=>{
+      document.querySelector('.controls .active').classList.remove('active');
+      btn.classList.add('active');
+      let src=btn.getAttribute('data-src');
+      document.querySelector('#video-slider').src = src;
+     });
+  });
+
+
+
+
+
+
